348d7100864f64e596e0acea80c08250b2d360e1
Neuer Nutzer <neuernutzer@w134-87-148-152.wireless.uvic.ca>
2017-09-21 00:50:45 +0200

    Tested the sort() method - worked. Also found out that methods can simply be attached to variables by a '.' like in VBA.

---
+++ p1_6-1.js
@@ -0,0 +1,3 @@
+var Array = ['BBB', 'EEE', 'CCC', 'DDD', 'AAA', 'FFF']
+var Output = Array.sort()  //seems like that works the same way as in VBA --> attach a method to a variabel with a simple "."
+alert(Output)
