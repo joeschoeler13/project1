36641117f40022bfeb99d95ec91143f93e919745
Neuer Nutzer <neuernutzer@w134-87-148-152.wireless.uvic.ca>
2017-09-21 00:43:13 +0200

    Finally found a sort-method (here: https://www.w3schools.com/jsref/jsref_obj_array.asp) and will test it in V6-1

---
+++ p1_6.js
@@ -0,0 +1,8 @@
+var Input = prompt("Please enter your words, separated by ', ' to let the program alphabetize them.")
+function alphabetize(words) {
+    //".split()"-method for splitting the whole string (from prompt) into substrings
+    //???put substrings into array?
+    //".sort()"-method tested in V6-1
+    //???how to do the output?
+}
+alert("These are your alphabetized words:" + alphabetize(Input))
