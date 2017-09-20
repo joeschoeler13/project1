ea5d7628f57757af7c2b3bb4dae9beca0ce4907c
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-20 19:55:51 +0200

    Framework for the function to be created, with starting prompt, function frame and ending alert-box

---
+++ p1_4.js
@@ -0,0 +1,5 @@
+var Input = prompt("Please enter your words, separated by ', ' to let the program alphabetize them.")
+function alphabetize(words) {
+	
+}
+alert("These are your alphabetized words:" + alphabetize(Input))
