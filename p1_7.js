345778af488e883fceb462a0f9fb22377560e622
Neuer Nutzer <neuernutzer@w134-87-148-152.wireless.uvic.ca>
2017-09-21 01:00:38 +0200

    Test if it works like I expected to just combine the methods and get a output. DID NOT.

---
+++ p1_7.js
@@ -0,0 +1,7 @@
+var Input = prompt("Please enter your words, separated by ', ' to let the program alphabetize them.")
+function alphabetize(words) {
+    words.split().sort()			//".split() && ".sort()"-method tested in V6-1
+    													//???put substrings into array?
+    													//???how to do the output?
+}
+alert("These are your alphabetized words:" + alphabetize(Input))
