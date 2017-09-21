bf1524cabe66dc45c57c4792f5c62640a1c52404
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-21 09:04:54 +0200

    Same code as in V12. I did not see options to tidy it up. This is the final version.

--- p1_final.js
+++ p1_final.js
@@ -1,5 +1,17 @@
-var Input = prompt("Please enter your words, separated by ', ' to let the program alphabetize them.")
-function alphabetize(words) {
-	return words.split(", ").sort().join(", ")
+var Input = prompt("enter words")
+var ConsHi = 12
+var ConsLo = 2
+
+function GiveBack(array) {
+  return array.split(", ").sort().length
 }
-alert("These are your alphabetized words: " + alphabetize(Input))
+var proof = GiveBack(Input)
+
+if (proof <= ConsHi && proof >= ConsLo) {
+  function alphabetize(words) {
+    return words.split(", ").sort().join(", ")
+  }
+  alert("These are your alphabetized words: " + alphabetize(Input))
+} else {
+  alert("Sorry, the amount of words you inserted did not fit the constraint of 2 to 12 words. Try again!")
+}
